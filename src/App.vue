d018b413c02aafdcf88ee3412b2b088ccbd19c4c
Caner Turna <turnacaner53@outlook.com>
2021-10-02 02:27:00 +0300

    final

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <template>
   <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+
+  <Results v-if="showResult" :score="score" />
 </template>
 
 <script>
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 1000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResult = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
+    },
+  },
 };
 </script>
 
@@ -17,5 +45,22 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled]{
+  opacity: 0.2;
+  cursor:not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reacion Time : {{ score }}ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Godlike";
+    } else if (this.score < 400) {
+      this.rank = "Fair enough";
+    } else {
+      this.rank = "Hi grandpa!";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  font-weight: bold;
+  color: #0faf87;
+}
+</style>
